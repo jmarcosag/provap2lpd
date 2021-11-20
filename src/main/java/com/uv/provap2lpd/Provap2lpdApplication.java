90646797c020afacc2c2079840eac39d3c4a344b
João Marcos <jotamarcosangelo@gmail.com>
2021-11-20 17:43:22 -0300

    Arquivos iniciais

---
+++ src/main/java/com/uv/provap2lpd/Provap2lpdApplication.java
@@ -0,0 +1,13 @@
+package com.uv.provap2lpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provap2lpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provap2lpdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uv/provap2lpd/Provap2lpdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uv.provap2lpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provap2lpdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
